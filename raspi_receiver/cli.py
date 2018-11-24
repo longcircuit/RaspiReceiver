7a2f9f5f755a0e88d806ae6d2b4519931e33d1a3
Stefano Borini <stefano.borini@gmail.com>
2018-11-24 15:13:36 +0000

    Added code to cli

--- raspi_receiver/cli.py
+++ raspi_receiver/cli.py
@@ -1,2 +1,30 @@
+from rfm69py.RFM69 import RFM69
+from rfm69py import RFM69registers
+import datetime
+import time
+import struct
+
+
+
+def loop():
+    radio = RFM69(RF69_433MHZ, 1, 100, False)
+    try:
+        while True:
+            radio.receiveBegin()
+            while not radio.receiveDone():
+                time.sleep(.1)
+
+            if radio.ACKRequested():
+                radio.sendACK()
+    finally:
+        radio.shutdown()
+
+
 def main():
-    print("RaspiReceiver")
+    print("RaspiReceiver started")
+
+    try:
+        loop()
+    except KeyboardInterrupt:
+      print("You've exited the program")
+
